6941407980a2cbea4793c5e46674aac8896aed39
renegng <renegng@gmail.com>
2020-06-02 20:02:38 -0600

    Nuevos boletines y webinar MAA

--- static/js/sw.js
+++ static/js/sw.js
@@ -4,11 +4,11 @@
 
 const filesToPreCache = [
     // Web pages
-    { url: '/', revision: '2020-05-27-1' },
+    { url: '/', revision: '2020-06-02-1' },
     { url: '/acercade/', revision: '2019-05-28-1' },
-    { url: '/boletines/', revision: '2020-05-21-2' },
+    { url: '/boletines/', revision: '2020-06-02-1' },
     { url: '/cmvirtual/', revision: '2020-05-13-1' },
-    { url: '/cmvirtual/maa/', revision: '2020-04-28-1' },
+    { url: '/cmvirtual/maa/', revision: '2020-06-02-1' },
     { url: '/cmvirtual/mae/', revision: '2020-05-18-2' },
     { url: '/cmvirtual/maprodem/', revision: '2020-05-27-1' },
     { url: '/cmvirtual/mssr/', revision: '2020-05-13-1' },
