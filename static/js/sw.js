f60f67f6962d3f4954d5bae93ac1a190d6fd49e5
renegng <renegng@gmail.com>
2020-06-21 22:30:29 -0600

    Nueva noticia y boletin

--- static/js/sw.js
+++ static/js/sw.js
@@ -4,12 +4,12 @@
 
 const filesToPreCache = [
     // Web pages
-    { url: '/', revision: '2020-06-19-1' },
+    { url: '/', revision: '2020-06-21-1' },
     { url: '/acercade/', revision: '2019-05-28-1' },
-    { url: '/boletines/', revision: '2020-06-19-1' },
-    { url: '/cmvirtual/', revision: '2020-06-19-1' },
+    { url: '/boletines/', revision: '2020-06-21-1' },
+    { url: '/cmvirtual/', revision: '2020-06-21-1' },
     { url: '/cmvirtual/maa/', revision: '2020-06-02-1' },
-    { url: '/cmvirtual/mae/', revision: '2020-06-09-1' },
+    { url: '/cmvirtual/mae/', revision: '2020-06-21-1' },
     { url: '/cmvirtual/mai/', revision: '2020-06-19-1' },
     { url: '/cmvirtual/maprodem/', revision: '2020-05-27-1' },
     { url: '/cmvirtual/mssr/', revision: '2020-05-13-1' },
