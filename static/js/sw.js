2b5861e1406bd5c12ad063ebae6580383114e57d
renegng <renegng@gmail.com>
2020-10-01 18:55:37 -0600

    Nuevo boletin mensual

--- static/js/sw.js
+++ static/js/sw.js
@@ -4,9 +4,9 @@
 
 const filesToPreCache = [
     // Web pages
-    { url: '/', revision: '2020-09-29-1' },
+    { url: '/', revision: '2020-10-01-1' },
     { url: '/acercade/', revision: '2019-05-28-1' },
-    { url: '/boletines/', revision: '2020-09-29-1' },
+    { url: '/boletines/', revision: '2020-10-01-1' },
     { url: '/cmvirtual/', revision: '2020-09-29-1' },
     { url: '/cmvirtual/maa/', revision: '2020-06-02-1' },
     { url: '/cmvirtual/mae/', revision: '2020-09-29-1' },
